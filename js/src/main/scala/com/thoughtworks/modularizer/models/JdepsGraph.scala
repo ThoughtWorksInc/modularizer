0bc0e1cc0ea3f1e0c2814d397e1ce47972a5871f
Yang Bo <atryyang@thoughtworks.com>
2019-04-22 14:51:23 +0800

    Support jdeps DOT file created by JDK 8

--- js/src/main/scala/com/thoughtworks/modularizer/models/JdepsGraph.scala
+++ js/src/main/scala/com/thoughtworks/modularizer/models/JdepsGraph.scala
@@ -5,7 +5,7 @@ import typings.graphlibLib.graphlibMod.{Graph, GraphOptions}
 import scala.util.matching.Regex
 
 object JdepsGraph {
-  private val BeforeSpace: Regex = """^(\S+) .*$""".r
+  private val BeforeSpace: Regex = """^(\S+)(?:\s.*)?$""".r
 }
 
 import com.thoughtworks.modularizer.models.JdepsGraph._
@@ -22,39 +22,40 @@ final case class JdepsGraph(jdepsGraph: Graph) extends AnyVal {
     * @example Given a graph parsed from a DOT file,
     *
     *          {{{
-    *                    import typings.graphlibDashDotLib.graphlibDashDotMod
-    *                    import com.thoughtworks.modularizer.models.JdepsGraph
-    *                    val jdepsGraph = JdepsGraph(graphlibDashDotMod.^.read("""
-    *                      digraph "your.jar" {
-    *                          // Path: your/target/your.jar
-    *                          "com.foo.bar"     -> "java.io (java.base)";
-    *                          "com.foo.bar"     -> "com.foo.bar.baz (your.jar)";
-    *                          "com.foo.bar.baz" -> "com.notfound.mypackage (找不到)";
-    *                      }
-    *                    """))
+    *          import typings.graphlibDashDotLib.graphlibDashDotMod
+    *          import com.thoughtworks.modularizer.models.JdepsGraph
+    *          val jdepsGraph = JdepsGraph(graphlibDashDotMod.^.read("""
+    *            digraph "your.jar" {
+    *              // Path: your/target/your.jar
+    *              "com.foo.bar"     -> "java.io (java.base)";
+    *              "com.foo.bar"     -> "com.foo.bar.baz (your.jar)";
+    *              "com.foo.bar.baz" -> "com.notfound.mypackage (找不到)";
+    *              "com.foo.bar"     -> "com.notfound.mypackage";
+    *            }
+    *          """))
     *          }}}
     *
     *          when calculating internal dependencies,
     *
     *          {{{
-    *                    val internalDependencies = jdepsGraph.internalDependencies
+    *          val internalDependencies = jdepsGraph.internalDependencies
     *          }}}
     *
     *          then the returned graph should only contain internal dependencies,
     *          and package names in parentheses should be removed.
     *
     *          {{{
-    *                    import scala.scalajs.js
+    *          import scala.scalajs.js
     *
-    *                    new js.WrappedArray(internalDependencies.nodes) should contain only ("com.foo.bar", "com.foo.bar.baz")
+    *          new js.WrappedArray(internalDependencies.nodes) should contain only ("com.foo.bar", "com.foo.bar.baz")
     *
-    *                    import org.scalatest.Inside._
-    *                    import typings.graphlibLib.graphlibMod.Edge
-    *                    inside(new js.WrappedArray(internalDependencies.edges)) {
-    *                      case js.WrappedArray(edge) =>
-    *                        edge.v should be("com.foo.bar")
-    *                        edge.w should be("com.foo.bar.baz")
-    *                    }
+    *          import org.scalatest.Inside._
+    *          import typings.graphlibLib.graphlibMod.Edge
+    *          inside(new js.WrappedArray(internalDependencies.edges)) {
+    *            case js.WrappedArray(edge) =>
+    *              edge.v should be("com.foo.bar")
+    *              edge.w should be("com.foo.bar.baz")
+    *          }
     *          }}}
     */
   def internalDependencies: Graph = {
