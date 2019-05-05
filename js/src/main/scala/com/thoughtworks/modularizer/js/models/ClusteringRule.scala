f55726e0d2a67512a0d6e00218d5fc4b328d9040
Yang Bo <atryyang@thoughtworks.com>
2019-05-05 18:09:22 +0800

    Add comments

--- js/src/main/scala/com/thoughtworks/modularizer/js/models/ClusteringRule.scala
+++ js/src/main/scala/com/thoughtworks/modularizer/js/models/ClusteringRule.scala
@@ -2,15 +2,18 @@ package com.thoughtworks.modularizer.js.models
 import scala.collection.immutable
 import upickle.default._
 
-/**
+/** The root type of `rule.json`
+  *
   * @author 杨博 (Yang Bo)
   */
-final case class ClusteringRule(breakingEdges: Iterable[(String, String)], clusters: immutable.Seq[ClusteringRule.Cluster]) {}
+final case class ClusteringRule(breakingEdges: Iterable[(String, String)],
+                                clusters: immutable.Seq[ClusteringRule.Cluster]) {}
 object ClusteringRule {
   def empty = ClusteringRule(Iterable.empty, immutable.Seq.empty)
 
   implicit val rw: ReadWriter[ClusteringRule] = macroRW
 
+  // TODO: Rename to ClusterLocking
   final case class Cluster(parent: String, children: immutable.Seq[String])
   object Cluster {
     implicit val rw: ReadWriter[Cluster] = macroRW
